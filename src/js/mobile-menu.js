ca2c57e38a22502e93a090fc741752642a0c2b98
OsMax <www.osmax@gmail.com>
2023-05-21 13:06:14 +0300

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('#mobile-menu');
   const openMenuBtn = document.querySelector('#open-menu');
   const closeMenuBtn = document.querySelector('#close-menu');
+  const changeHeaderNav = document.querySelector('#change-header-nav');
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
@@ -9,5 +10,7 @@
   function toggleMenu() {
     document.body.classList.toggle('modal-open');
     mobileMenu.classList.toggle('is-open');
+    openMenuBtn.classList.toggle('is-open');
+    changeHeaderNav.classList.toggle('is-open');
   }
 })();
